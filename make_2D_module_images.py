2630dc4cc4f0cdbaee59c120b4d94c0a8ea41b8d
cwilkinson <cwilkinson@lbl.gov>
2024-02-16 17:34:20 -0800

    Make into a general script

--- make_2D_module_images.py
+++ make_2D_module_images.py
@@ -1,43 +1,27 @@
+import sys
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from numba import njit
 
-## This is a pretty ugly function, but... it works
-def get_pixel_numz(z_pos):
-    z_min = -30.816299438476562   
-    pixel_pitch = 0.4434
-    z_pixel = int((z_pos - z_min)/0.4424 + pixel_pitch/10.)
-    return z_pixel
-
-def get_pixel_numy(y_pos):
-    y_min = -83.67790222167969
-    pixel_pitch = 0.4434
-    y_pixel = int((y_pos - y_min)/0.4424 + pixel_pitch/10.)
-    return y_pixel
-
-
-#filename = "/global/cfs/cdirs/dune/www/data/Module1/TPC12/reflow-test/flowed_v1/packet_2022_02_11_11_39_26_CET_0cd913fb_20220211_113926.data.module1_flow.h5"
-filename = "inputs/packet_2022_02_08_07_36_25_CET_0cd913fb_20220208_073625.data.module1_flow.h5"
-
 show_plots = False
 
 @njit
 def make_image(these_hits):
+
     ## Set up an "image" with 140x280 pixels
     this_image = np.zeros((280,140))
-    nnan = 0
     for hit in these_hits:
         
         ## Check for NaNs
         if np.isnan(hit['Q']) or \
            np.isnan(hit['y']) or \
            np.isnan(hit['z']):
-            nnan += 1
             continue
         
         ## Get pixel numbers and charge values
+        ## (Ugly but it's compiled!)
         z_min = -30.816299438476562   
         pixel_pitch = 0.4434
         this_z = int((hit['z'] - z_min)/0.4424 + pixel_pitch/10.)
@@ -51,9 +35,11 @@ def make_image(these_hits):
         ## Add to image LarPix(z,y) = image(x,y)
         this_image[this_y, this_z] += this_q
         
-    return this_image, nnan
+    return this_image
 
-with h5py.File(filename, "r") as f:
+def make_images(input_file_name, output_file_name):
+
+    f = h5py.File(input_file_name, "r")
 
     ## Allows a minimum number of hits to be an interesting event
     min_hits = 1
@@ -69,12 +55,11 @@ with h5py.File(filename, "r") as f:
     hits_ref = f['charge/events/ref/charge/calib_prompt_hits/ref']
     hits_region = f['charge/events/ref/charge/calib_prompt_hits/ref_region']
 
+    print("With total:", len(hits), "hits")
+    
     ## This is what we'll eventually save
     list_of_images = []
     
-    ntotal = 0
-    nnan = 0
-
     if show_plots:
         plt.ion()
         plt.figure(figsize=(4.5, 7))
@@ -89,41 +74,12 @@ with h5py.File(filename, "r") as f:
         hit_ref = np.sort(hit_ref[hit_ref[:,0] == ev_id, 1])
         these_hits = hits[hit_ref]
 
-        ## Set up an "image" with 140x280 pixels
-        this_image = np.zeros((280,140))
-
-        ntotal += len(these_hits)
-
-        if len(these_hits) == 0:
-            print("Skipping!")
-            continue
-        
-        ## Naively think I have to loop over hits
-        ## I *think* I can probably use hit_ref to filter these_hits to get a list of all Q that correspond to 
-        for hit in these_hits:
-        
-            ## Check for NaNs
-            if np.isnan(hit['Q']) or \
-               np.isnan(hit['y']) or \
-               np.isnan(hit['z']):
-                nnan += 1
-                continue
-        
-            ## Get pixel numbers and charge values
-            this_y = get_pixel_numy(hit['y'])
-            this_z = get_pixel_numz(hit['z'])
-            this_q = hit['Q']
-            
-            ## Add to image LarPix(z,y) = image(x,y)
-            this_image[this_y, this_z] += this_q
-
-        # this_image, this_nnan = make_image(these_hits)
-        # nnan += this_nnan
+        ## Get an image with 140x280 pixels
+        this_image = make_image(these_hits)
         
         if show_plots:
             ## Show image temporarily
             plt.imshow(this_image, origin='lower')
-            # plt.savefig("image"+str(evt)+".png")
             plt.show(block=False)
             input("Continue...")
 
@@ -133,11 +89,20 @@ with h5py.File(filename, "r") as f:
     batch_data  = np.asarray(list_of_images)
     batch_index = np.array(range(len(list_of_images)))
     
-    output_file = h5py.File('output_file.h5','w')
+    output_file = h5py.File(output_file_name,'w')
     output_file.create_dataset('data',data=batch_data, chunks=tuple([1]+list(batch_data[0].shape)), compression='gzip')
     output_file.create_dataset('index',data=batch_index, chunks=batch_index.shape, compression='gzip')
     output_file.close()
 
-    
-    print("Found", nnan, "/", ntotal, "nans")
-    
+    f.close()
+
+if __name__ == '__main__':
+
+    ## Take an input file and convert it to an h5 file of images
+    if len(sys.argv) < 3:
+        print("An input file and output file name must be provided as arguments!")
+        sys.exit()
+
+    input_file_name = sys.argv[1]
+    output_file_name = sys.argv[2]
+    make_images(input_file_name, output_file_name)
