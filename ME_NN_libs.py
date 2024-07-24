78ba977c036f920a07df04b66834201124efb224
Callum Wilkinson <cwilkinson@lbl.gov>
2024-07-24 00:43:24 -0700

    Minor changes

--- ME_NN_libs.py
+++ ME_NN_libs.py
@@ -471,89 +471,89 @@ class DecoderME(nn.Module):
         )
         
         ## Split into blocks
-        self.block1 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[7], out_channels=self.ch[6], kernel_size=2, stride=2, bias=False, dimension=2), ## 1x1 ==> 2x2
-            ME.MinkowskiBatchNorm(self.ch[6]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[6], out_channels=self.ch[6], kernel_size=3, stride=1, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[6]),
-            act_fn(), 
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[6], out_channels=self.ch[5], kernel_size=2, stride=2, bias=False, dimension=2), ## 2x2 ==> 4x4
-            ME.MinkowskiBatchNorm(self.ch[5]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[5], out_channels=self.ch[5], kernel_size=3, stride=1, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[5]),
-            act_fn()
-        )
-        self.block1_cls = ME.MinkowskiConvolution(
-            self.ch[5], 1, kernel_size=1, bias=True, dimension=2
-        )
-        self.block2 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[5], out_channels=self.ch[4], kernel_size=2, stride=2, bias=False, dimension=2), ## 4x4 ==> 8x8
-            ME.MinkowskiBatchNorm(self.ch[4]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[4], out_channels=self.ch[4], kernel_size=3, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[4]),
-            act_fn()
-        )
-        self.block2_cls = ME.MinkowskiConvolution(
-            self.ch[4], 1, kernel_size=1, bias=True, dimension=2
-        )
-        
-        self.block3 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[4], out_channels=self.ch[3], kernel_size=2, stride=2, bias=False, dimension=2), ## 8x8 ==> 16x16
-            ME.MinkowskiBatchNorm(self.ch[3]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[3], out_channels=self.ch[3], kernel_size=3, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[3]),
-            act_fn()
-        )
-        self.block3_cls = ME.MinkowskiConvolution(
-            self.ch[3], 1, kernel_size=1, bias=True, dimension=2
-        )
-            
-        self.block4 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[3], out_channels=self.ch[2], kernel_size=2, stride=2, bias=False, dimension=2), ## 16x16 ==> 32x32
-            ME.MinkowskiBatchNorm(self.ch[2]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[2], out_channels=self.ch[2], kernel_size=3, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[2]),
-            act_fn()
-        )
-        self.block4_cls = ME.MinkowskiConvolution(
-            self.ch[2], 1, kernel_size=1, bias=True, dimension=2
-        )
-        
-        self.block5 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[2], out_channels=self.ch[1], kernel_size=2, stride=2, bias=False, dimension=2), ## 32x32 ==> 64x64
-            ME.MinkowskiBatchNorm(self.ch[1]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[1], out_channels=self.ch[1], kernel_size=3, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[1]),
-            act_fn()
-        )
-        self.block5_cls = ME.MinkowskiConvolution(
-            self.ch[1], 1, kernel_size=1, bias=True, dimension=2
-        )
-        
-        self.block6 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[1], out_channels=self.ch[0], kernel_size=2, stride=2, bias=False, dimension=2), ## 64x64 ==> 128x128
-            ME.MinkowskiBatchNorm(self.ch[0]),
-            act_fn(),
-            ME.MinkowskiConvolution(in_channels=self.ch[0], out_channels=self.ch[0], kernel_size=3, bias=False, dimension=2), ## No change in size
-            ME.MinkowskiBatchNorm(self.ch[0]),
-            act_fn(),          
-        )
-        self.block6_cls = ME.MinkowskiConvolution(
-            self.ch[0], 1, kernel_size=1, bias=True, dimension=2
-        )
-        self.block7 = nn.Sequential(
-            ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[0], out_channels=1, kernel_size=(2,1), stride=(2,1), bias=True, dimension=2), ## 128x128 ==> 256x128
-            act_fn()
-        )
-
-        ## For removing points
-        self.pruning = ME.MinkowskiPruning()
+        ## self.block1 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[7], out_channels=self.ch[6], kernel_size=2, stride=2, bias=False, dimension=2), ## 1x1 ==> 2x2
+        ##     ME.MinkowskiBatchNorm(self.ch[6]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[6], out_channels=self.ch[6], kernel_size=3, stride=1, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[6]),
+        ##     act_fn(), 
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[6], out_channels=self.ch[5], kernel_size=2, stride=2, bias=False, dimension=2), ## 2x2 ==> 4x4
+        ##     ME.MinkowskiBatchNorm(self.ch[5]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[5], out_channels=self.ch[5], kernel_size=3, stride=1, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[5]),
+        ##     act_fn()
+        ## )
+        ## self.block1_cls = ME.MinkowskiConvolution(
+        ##     self.ch[5], 1, kernel_size=1, bias=True, dimension=2
+        ## )
+        ## self.block2 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[5], out_channels=self.ch[4], kernel_size=2, stride=2, bias=False, dimension=2), ## 4x4 ==> 8x8
+        ##     ME.MinkowskiBatchNorm(self.ch[4]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[4], out_channels=self.ch[4], kernel_size=3, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[4]),
+        ##     act_fn()
+        ## )
+        ## self.block2_cls = ME.MinkowskiConvolution(
+        ##     self.ch[4], 1, kernel_size=1, bias=True, dimension=2
+        ## )
+        ## 
+        ## self.block3 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[4], out_channels=self.ch[3], kernel_size=2, stride=2, bias=False, dimension=2), ## 8x8 ==> 16x16
+        ##     ME.MinkowskiBatchNorm(self.ch[3]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[3], out_channels=self.ch[3], kernel_size=3, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[3]),
+        ##     act_fn()
+        ## )
+        ## self.block3_cls = ME.MinkowskiConvolution(
+        ##     self.ch[3], 1, kernel_size=1, bias=True, dimension=2
+        ## )
+        ##     
+        ## self.block4 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[3], out_channels=self.ch[2], kernel_size=2, stride=2, bias=False, dimension=2), ## 16x16 ==> 32x32
+        ##     ME.MinkowskiBatchNorm(self.ch[2]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[2], out_channels=self.ch[2], kernel_size=3, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[2]),
+        ##     act_fn()
+        ## )
+        ## self.block4_cls = ME.MinkowskiConvolution(
+        ##     self.ch[2], 1, kernel_size=1, bias=True, dimension=2
+        ## )
+        ## 
+        ## self.block5 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[2], out_channels=self.ch[1], kernel_size=2, stride=2, bias=False, dimension=2), ## 32x32 ==> 64x64
+        ##     ME.MinkowskiBatchNorm(self.ch[1]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[1], out_channels=self.ch[1], kernel_size=3, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[1]),
+        ##     act_fn()
+        ## )
+        ## self.block5_cls = ME.MinkowskiConvolution(
+        ##     self.ch[1], 1, kernel_size=1, bias=True, dimension=2
+        ## )
+        ## 
+        ## self.block6 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[1], out_channels=self.ch[0], kernel_size=2, stride=2, bias=False, dimension=2), ## 64x64 ==> 128x128
+        ##     ME.MinkowskiBatchNorm(self.ch[0]),
+        ##     act_fn(),
+        ##     ME.MinkowskiConvolution(in_channels=self.ch[0], out_channels=self.ch[0], kernel_size=3, bias=False, dimension=2), ## No change in size
+        ##     ME.MinkowskiBatchNorm(self.ch[0]),
+        ##     act_fn(),          
+        ## )
+        ## self.block6_cls = ME.MinkowskiConvolution(
+        ##     self.ch[0], 1, kernel_size=1, bias=True, dimension=2
+        ## )
+        ## self.block7 = nn.Sequential(
+        ##     ME.MinkowskiGenerativeConvolutionTranspose(in_channels=self.ch[0], out_channels=1, kernel_size=(2,1), stride=(2,1), bias=True, dimension=2), ## 128x128 ==> 256x128
+        ##     act_fn()
+        ## )
+        ## 
+        ## ## For removing points
+        ## self.pruning = ME.MinkowskiPruning()
         
         # Initialize weights using Xavier initialization
         self.initialize_weights()
@@ -570,79 +570,79 @@ class DecoderME(nn.Module):
                     nn.init.constant_(m.bn.bias, 0)
              
     ## This is definitely not working as expected. It only ever gives a single True value...
-    @torch.no_grad()
-    def get_target(self, out, target_key, kernel_size=1):
-        target = torch.zeros(len(out), dtype=torch.bool, device=out.device)
-        cm = out.coordinate_manager
-        strided_target_key = cm.stride(
-            target_key,
-            out.tensor_stride
-        )
-        kernel_map = cm.kernel_map(
-            out.coordinate_map_key,
-            strided_target_key,
-            kernel_size=kernel_size,
-            region_type=1,
-        )
-        for k, curr_in in kernel_map.items():
-            target[curr_in[0].long()] = 1
-        return target
+    ## @torch.no_grad()
+    ## def get_target(self, out, target_key, kernel_size=1):
+    ##     target = torch.zeros(len(out), dtype=torch.bool, device=out.device)
+    ##     cm = out.coordinate_manager
+    ##     strided_target_key = cm.stride(
+    ##         target_key,
+    ##         out.tensor_stride
+    ##     )
+    ##     kernel_map = cm.kernel_map(
+    ##         out.coordinate_map_key,
+    ##         strided_target_key,
+    ##         kernel_size=kernel_size,
+    ##         region_type=1,
+    ##     )
+    ##     for k, curr_in in kernel_map.items():
+    ##         target[curr_in[0].long()] = 1
+    ##     return target
 
     def forward(self, x, target_key=None):
         x = self.decoder_lin(x)
         x = self.decoder_conv(x)
         return x
     
-    def test(self, x):
-                
-        out1 = self.block1(x)
-        out1_cls = self.block1_cls(out1)
-        keep1 = (out1_cls.F > 0).squeeze()
-        if self.training: 
-            target = self.get_target(out1, target_key,1)
-            keep1 += target
-        out1 = self.pruning(out1, keep1)
-        
-        out2 = self.block2(out1)
-        out2_cls = self.block2_cls(out2)
-        keep2 = (out2_cls.F > 0).squeeze()
-        if self.training: 
-            target = self.get_target(out2, target_key,1)
-            keep2 += target
-        out2 = self.pruning(out2, keep2)
-        
-        out3 = self.block3(out2)
-        out3_cls = self.block3_cls(out3)
-        keep3 = (out3_cls.F > 0).squeeze()
-        if self.training: 
-            target = self.get_target(out3, target_key,1)
-            keep3 += target
-        out3 = self.pruning(out3, keep3)
-        
-        out4 = self.block4(out3)
-        out4_cls = self.block4_cls(out4)
-        keep4 = (out4_cls.F > 0).squeeze()
-        if self.training: 
-            target = self.get_target(out4, target_key,1)
-            keep4 += target
-        out4 = self.pruning(out4, keep4)
-        
-        out5 = self.block5(out4)
-        out5_cls = self.block5_cls(out5)
-        keep5 = (out5_cls.F > 0).squeeze()
-        if self.training: 
-            target = self.get_target(out5, target_key)
-            keep5 += target
-        out5 = self.pruning(out5, keep5)
-        
-        out6 = self.block6(out5)
-        out6_cls = self.block6_cls(out6)
-        keep6 = (out6_cls.F > 0).squeeze()
-        if self.training: 
-            target = self.get_target(out6, target_key)
-            keep6 += target
-        out6 = self.pruning(out6, keep6)
-        
-        out7 = self.block7(out6)
-        
-        return out7
+    ## def test(self, x):
+    ##             
+    ##     out1 = self.block1(x)
+    ##     out1_cls = self.block1_cls(out1)
+    ##     keep1 = (out1_cls.F > 0).squeeze()
+    ##     if self.training: 
+    ##         target = self.get_target(out1, target_key,1)
+    ##         keep1 += target
+    ##     out1 = self.pruning(out1, keep1)
+    ##     
+    ##     out2 = self.block2(out1)
+    ##     out2_cls = self.block2_cls(out2)
+    ##     keep2 = (out2_cls.F > 0).squeeze()
+    ##     if self.training: 
+    ##         target = self.get_target(out2, target_key,1)
+    ##         keep2 += target
+    ##     out2 = self.pruning(out2, keep2)
+    ##     
+    ##     out3 = self.block3(out2)
+    ##     out3_cls = self.block3_cls(out3)
+    ##     keep3 = (out3_cls.F > 0).squeeze()
+    ##     if self.training: 
+    ##         target = self.get_target(out3, target_key,1)
+    ##         keep3 += target
+    ##     out3 = self.pruning(out3, keep3)
+    ##     
+    ##     out4 = self.block4(out3)
+    ##     out4_cls = self.block4_cls(out4)
+    ##     keep4 = (out4_cls.F > 0).squeeze()
+    ##     if self.training: 
+    ##         target = self.get_target(out4, target_key,1)
+    ##         keep4 += target
+    ##     out4 = self.pruning(out4, keep4)
+    ##     
+    ##     out5 = self.block5(out4)
+    ##     out5_cls = self.block5_cls(out5)
+    ##     keep5 = (out5_cls.F > 0).squeeze()
+    ##     if self.training: 
+    ##         target = self.get_target(out5, target_key)
+    ##         keep5 += target
+    ##     out5 = self.pruning(out5, keep5)
+    ##     
+    ##     out6 = self.block6(out5)
+    ##     out6_cls = self.block6_cls(out6)
+    ##     keep6 = (out6_cls.F > 0).squeeze()
+    ##     if self.training: 
+    ##         target = self.get_target(out6, target_key)
+    ##         keep6 += target
+    ##     out6 = self.pruning(out6, keep6)
+    ##     
+    ##     out7 = self.block7(out6)
+    ##     
+    ##     return out7
