Fix entropy scaling

ME_NN_libs.py
@@ -36,12 +36,13 @@ class ClusteringLossMerged(nn.Module):
         p_i = p_i/p_i.sum()
         p_j = p_j/p_j.sum()
 
+        ## Maybe don't divide by class_num?
         entropy_i = -torch.sum(p_i * torch.log(p_i + 1e-10))/math.log(class_num)
         entropy_j = -torch.sum(p_j * torch.log(p_j + 1e-10))/math.log(class_num)
 
         ne_loss = -0.5 * (entropy_i + entropy_j)
         
-        return loss + ne_loss
+        return loss + ne_loss*self.entropy_weight
 
     
 class NTXentMerged(nn.Module):
@@ -784,7 +785,7 @@ class CCEncoderFSD12x4Avg(nn.Module):
             ME.MinkowskiConvolution(in_channels=self.ch[5], out_channels=self.ch[5], kernel_size=3, bias=False, dimension=2), ## No change in size
             ME.MinkowskiBatchNorm(self.ch[5]),
             act_fn(),
-            ME.MinkowskiAvgPooling()           
+            ME.MinkowskiGlobalAvgPooling()           
 
         )
 