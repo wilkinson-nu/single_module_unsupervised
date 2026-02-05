005dd2eda6999782b62974329154e2e583238f52
Callum Wilkinson <cwilkinson@lbl.gov>
2026-02-05 15:50:15 -0800

    Work out some minor issues with the submission script

--- datasets/nularbox/batch_GENIEv3_G18_10a_00_000_EDEPSIM_2D_TEMPLATE.sh
+++ datasets/nularbox/batch_GENIEv3_G18_10a_00_000_EDEPSIM_2D_TEMPLATE.sh
@@ -26,26 +26,26 @@ INPUTS_DIR=${PWD}/MC_inputs
 GENIE_TUNE=G18_10a_00_000
 
 ## Where to do stuff
-tempDir=${SCRATCH}/${OUTFILE/.root/}_${SEED}
+tempDir=${SCRATCH}/${OUTFILE_ROOT}_${SEED}
 echo "Moving to SCRATCH: ${tempDir}"
 mkdir ${tempDir}
 cd ${tempDir}
 
 ## Get the necessary inputs
-cp ${INPUTS_DIR}/${TUNE}_splines.xml.gx .
+cp ${INPUTS_DIR}/${GENIE_TUNE}_splines.xml.gz .
 cp ${INPUTS_DIR}/${FLUX_FILE} .
 cp ${INPUTS_DIR}/${EDEP_MAC} .
 cp ${INPUTS_DIR}/${GEOM} .
 cp ${INPUTS_DIR}/../make_2D_nusim_images.py .
 
 echo "Starting gevgen..."
-shifter --entrypoint gevgen -n ${NEVENTS} -t ${TARG} -p ${NU_PDG} \
+shifter gevgen -n ${NEVENTS} -t ${TARG} -p ${NU_PDG} \
         --cross-sections ${GENIE_TUNE}_splines.xml.gz \
-        --tune ${TUNE} --seed ${SEED} \
+        --tune ${GENIE_TUNE} --seed ${SEED} \
         -f ${FLUX_FILE},${FLUX_HIST} -e ${E_MIN},${E_MAX} -o ${OUTFILE_ROOT}_GHEP.root
 
 echo "Converting to rootracker..."
-shifter --entrypoint gntpc -i ${OUTFILE_ROOT}_GHEP.root -f rootracker -o ${OUTFILE_ROOT}_GROO.root
+shifter gntpc -i ${OUTFILE_ROOT}_GHEP.root -f rootracker -o ${OUTFILE_ROOT}_GROO.root
 
 ## Copy back the GENIE output file
 if [ ! -d "${OUTDIR_ROOT}/GENIE" ]; then
@@ -60,7 +60,7 @@ sed -i "s/_RAND1_/$((SEED + 1))/g" ${EDEP_MAC}
 sed -i "s/_RAND2_/$((SEED + 2))/g" ${EDEP_MAC}
 
 echo "Running edep-sim..."
-shifter --entrypoint edep-sim -o ${OUTFILE_ROOT}_EDEPSIM.root \
+shifter edep-sim -o ${OUTFILE_ROOT}_EDEPSIM.root \
 	${EDEP_MAC} \
 	-e ${NEVENTS}
 
@@ -71,7 +71,7 @@ fi
 cp ${tempDir}/${OUTFILE_ROOT}_EDEPSIM.root ${OUTDIR_ROOT}/EDEPSIM/.
 
 echo "Prepare 2D images..."
-shifter --entrypoint python3 make_2D_nusim_images.py ${OUTFILE_ROOT}_EDEPSIM.root ${OUTFILE_ROOT}_IMAGES2D.h5
+shifter python3 make_2D_nusim_images.py ${OUTFILE_ROOT}_EDEPSIM.root ${OUTFILE_ROOT}_IMAGES2D.h5
 
 ## Copy back the images
 if [ ! -d "${OUTDIR_ROOT}/IMAGES2D" ]; then
