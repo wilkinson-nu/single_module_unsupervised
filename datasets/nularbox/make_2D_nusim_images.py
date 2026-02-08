f08e6a6f534586df3d9ff22868aabc96c8e97f2b
cwilkinson <cwilkinson@lbl.gov>
2026-02-07 16:41:38 -0800

    Split truth labels into a seprate file

--- datasets/nularbox/make_2D_nusim_images.py
+++ datasets/nularbox/make_2D_nusim_images.py
@@ -6,103 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from scipy.sparse import coo_matrix
-from enum import Enum, auto
 from collections import defaultdict
 import json
+from datasets.nularbox.truth_labels import LABEL_DTYPE_EXP, Topology, Mode
 
 ## This is not something to be taken lightly as it will dump out an image for every event...
 make_plots = False
-
-## Initial label types to store, to be clarified and then will need to be versioned (probably)
-LABEL_DTYPE_EXP = np.dtype([
-    ("cc",        np.bool_),
-    ("topology",  np.int8),
-    ("mode",      np.int8),
-    ("nneutron",  np.int8),
-    ("nantineut", np.int8),
-    ("nproton",   np.int8),    
-    ("nantiprot", np.int8),    
-    ("npipm",     np.int8),
-    ("npi0",      np.int8),
-    ("nkapm",     np.int8),
-    ("nka0",      np.int8),
-    ("nem",       np.int8),
-    ("nmuon",     np.int8),    
-    ("nstrange",  np.int8),
-    ("ncharm",    np.int8),    
-    ("enu",       np.float32),
-    ("q0",        np.float32),
-])
-
-class Topology(Enum):
-
-    ## Default
-    NONE = -1
-
-    ## CC topologies
-    CC0pi = auto()
-    CC1pi0 = auto()
-    CC1pipm = auto()
-    CC2pi = auto()
-    CCNpi = auto()
-    CCOther = auto()
-
-    ## NC topologies
-    NC0pi = auto()
-    NC1pipm = auto()
-    NC1pi0 = auto()
-    NC2pi = auto()
-    NCNpi = auto()
-    NCOther = auto()
-    
-    ## A method to dump the list
-    @classmethod
-    def print_members(cls):
-        for member in cls:
-            print(f"{member.name}: {member.value}")
-
-    @classmethod
-    def name_from_index(cls, index):
-        for member in cls:
-            if member.value == index:
-                return member.name
-        return f"Unknown label for index {index}"
-
-class Mode(Enum):
-
-    ## Default
-    NONE = -1
-
-    ## CC modes
-    CCQE = auto()
-    CC2p2h = auto()
-    CCRES = auto()
-    CCDIS = auto()
-    CCCOH = auto()
-
-    ## NC modes
-    NCQE = auto()
-    NC2p2h = auto()
-    NCRES = auto()
-    NCDIS = auto()
-    NCCOH = auto()
-
-    ## Other
-    IMD = auto()
-    NUEE = auto()
-    
-    ## A method to dump the list
-    @classmethod
-    def print_members(cls):
-        for member in cls:
-            print(f"{member.name}: {member.value}")
-
-    @classmethod
-    def name_from_index(cls, index):
-        for member in cls:
-            if member.value == index:
-                return member.name
-        return f"Unknown label for index {index}"
 
 def get_mode(code):
 
