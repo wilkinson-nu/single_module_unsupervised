91ded0fcbf1237e738c8d775fe40c902d2f393a0
Callum Wilkinson <cwilkinson@lbl.gov>
2026-02-25 17:05:04 -0800

    Switch to resnet blocks by default

--- analysis/model_utils.py
+++ analysis/model_utils.py
@@ -3,7 +3,8 @@ import argparse
 
 ## Temporarily override for testing
 # from core.models.encoder import get_encoder
-from datasets.nularbox.encoder import get_encoder
+# from datasets.nularbox.encoder import get_encoder
+from datasets.nularbox.resnet_encoder import get_encoder
 from core.models.projection_head import get_projhead
 from core.models.clustering_head import get_clusthead
 
