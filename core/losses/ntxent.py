08e714bddcc3545c50cc68dd85e1b569c7664fe6
Callum Wilkinson <cwilkinson@lbl.gov>
2026-05-05 16:30:15 -0700

    Some speed-ups

--- core/losses/ntxent.py
+++ core/losses/ntxent.py
@@ -7,6 +7,7 @@ class NTXentMerged(nn.Module):
     def __init__(self, temperature=0.5):
         super().__init__()
         self.temperature = temperature
+        self.cross_entropy = nn.CrossEntropyLoss()
 
     def forward(self, emb_cat):
         """
@@ -14,24 +15,16 @@ class NTXentMerged(nn.Module):
         """
         batch_size = emb_cat.shape[0]//2
         z_cat = nn.functional.normalize(emb_cat, dim=1)
-        z_i, z_j = z_cat[:batch_size], z_cat[batch_size:]
 
-        negatives_mask = (~torch.eye(batch_size*2, batch_size*2, dtype=bool, device=emb_cat.device)).float()
-        representations = torch.cat([z_i, z_j], dim=0)
-
-        z = nn.functional.normalize(representations, dim=1)
-        similarity_matrix = torch.mm(z, z.t())
-
-        sim_ij = torch.diag(similarity_matrix, batch_size)
-        sim_ji = torch.diag(similarity_matrix, -batch_size)
-        positives = torch.cat([sim_ij, sim_ji], dim=0)
-
-        nominator = torch.exp(positives / self.temperature)
-        denominator = negatives_mask * torch.exp(similarity_matrix / self.temperature)
-
-        loss_partial = -torch.log(nominator / torch.sum(denominator, dim=1))
-        loss = torch.mean(loss_partial)
-
+        sim = torch.mm(z_cat, z_cat.t()) / self.temperature
+        mask = torch.eye(2*batch_size, device=z_cat.device, dtype=torch.bool)
+        sim.masked_fill_(mask, -float("inf"))
+        
+        positives = torch.arange(2*batch_size, device=z_cat.device)
+        positives = (positives + batch_size) % (2*batch_size)
+        
+        loss = self.cross_entropy(sim, positives)
+        
         return loss
 
 
@@ -46,8 +39,8 @@ class NTXentMergedMultiGPU(nn.Module):
         emb_cat: concatenated embeddings of shape (2*B_per_gpu, D), stacked as [z_i, z_j]
         """
 
-        if not torch.isfinite(emb_cat).all():
-            print("Projection head output contains NaN/Inf")
+        #if not torch.isfinite(emb_cat).all():
+        #    print("Projection head output contains NaN/Inf")
         
         batch_size = emb_cat.shape[0]//2
         z_cat = emb_cat / (emb_cat.norm(dim=1, keepdim=True) + 1e-8)
@@ -60,10 +53,10 @@ class NTXentMergedMultiGPU(nn.Module):
 
         z_all = torch.cat([z_i_all, z_j_all], dim=0)
 
-        if not torch.isfinite(z_all).all():
-            print("Embedding contains NaN/Inf")
-            print(z_all.min(), z_all.max())
-            raise RuntimeError("Bad embeddings")
+        #if not torch.isfinite(z_all).all():
+        #    print("Embedding contains NaN/Inf")
+        #    print(z_all.min(), z_all.max())
+        #    raise RuntimeError("Bad embeddings")
         
         sim = torch.mm(z_all, z_all.t()) / self.temperature
         mask = torch.eye(2*total_batch, device=z_all.device, dtype=torch.bool)
