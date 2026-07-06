cff17e7eef3b8e352d0d8e17413056ecdea6b07b
cwilkinson <cwilkinson@lbl.gov>
2026-07-06 14:04:05 -0700

    Fix minor typo

--- datasets/nularbox/resnetv1_blocks.py
+++ datasets/nularbox/resnetv1_blocks.py
@@ -67,7 +67,7 @@ class BasicBlock(nn.Module):
         if self.norm1 is not None: out = self.norm1(out)
         out = self.act_fn(out)
         out = self.conv2(out)
-        if self.norm1 is not None: out = self.norm2(out)
+        if self.norm2 is not None: out = self.norm2(out)
         out += residual
         out = self.act_fn(out)
         return out
