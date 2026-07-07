699d35cd6afe490f323e8d8f1d84b105c9aa9832
cwilkinson <cwilkinson@lbl.gov>
2026-07-06 22:47:34 -0700

    Add a couple of new augmentation test options

--- datasets/nularbox/augmentations_2d.py
+++ datasets/nularbox/augmentations_2d.py
@@ -306,7 +306,32 @@ def get_transform(image_size=256, aug_type=None, aug_prob=1, aug_val=None):
             aug.RandomDropout(0.1, p=aug_prob),
             RandomCenterCrop([y_orig,x_orig], [y_max,x_max], [256, 192], 10),
 	])
+    if aug_type == "v0prethreshsplat":
+        return transforms.Compose([
+            aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
+            aug.GridJitter(2, 0.1),
+            aug.JitterCoords(0.1),
+            aug.RandomScaleCharge(0.05, p=aug_prob),
+            aug.RandomJitterCharge(0.05, p=aug_prob),
+            LogAlphaCharge(5),
+            aug.BilinearSplatPreThreshold(0.1, 0.4),
+            aug.RandomDropout(0.1, p=aug_prob),
+            RandomCenterCrop([y_orig,x_orig], [y_max,x_max], [256, 192], 10),
+	])
 
+    if aug_type == "v0testsplatfix":
+        return transforms.Compose([
+            aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
+            aug.GridJitter(2, 0.1),
+            aug.JitterCoords(0.1),
+            aug.RandomScaleCharge(0.05, p=aug_prob),
+            aug.RandomJitterCharge(0.05, p=aug_prob),
+            LogAlphaCharge(5),
+            aug.BilinearSplatModFIX(0.1, 0.4),
+            aug.RandomDropout(0.1, p=aug_prob),
+            RandomCenterCrop([y_orig,x_orig], [y_max,x_max], [256, 192], 10),
+	])    
+    
     if aug_type == "v0ongrid":
         return transforms.Compose([
             aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
