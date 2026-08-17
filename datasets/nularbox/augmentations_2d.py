291acb27d72ddf0368a9cc21fae74b09319a9458
Callum Wilkinson <cwilkinson@lbl.gov>
2026-08-17 16:59:02 -0700

    Add a couple more augmentation options

--- datasets/nularbox/augmentations_2d.py
+++ datasets/nularbox/augmentations_2d.py
@@ -301,6 +301,54 @@ def get_transform(image_size=256, aug_type=None, aug_prob=1, aug_val=None):
 	])
 
 
+    if aug_type == "v0m":
+        return transforms.Compose([
+            aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
+            aug.GridJitter(2, 0.1),
+            aug.JitterCoords(0.1),
+            RandomCentralRotation2D(20, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            RandomCentralShear2D(0.2, 0.2, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            RandomCentralStretch2D(0.1, 0.1, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            aug.RandomScaleCharge(0.05, p=aug_prob),
+            aug.RandomJitterCharge(0.05, p=aug_prob),
+            LogAlphaCharge(5),
+            aug.BilinearSplatReduce(0.25, 0.4),
+            aug.RandomDropout(0.2, p=aug_prob),
+            RandomCenterCrop([y_orig,x_orig], [y_max,x_max], [256, 192], 10),
+        ])
+
+    if aug_type == "v0l":
+        return transforms.Compose([
+            aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
+            aug.GridJitter(2, 0.1),
+            aug.JitterCoords(0.1),
+            RandomCentralRotation2D(40, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            RandomCentralShear2D(0.3, 0.3, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            RandomCentralStretch2D(0.2, 0.2, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            aug.RandomScaleCharge(0.1, p=aug_prob),
+            aug.RandomJitterCharge(0.1, p=aug_prob),
+            LogAlphaCharge(5),
+            aug.BilinearSplatReduce(0.25, 0.4),
+            aug.RandomDropout(0.3, p=aug_prob),
+            RandomCenterCrop([y_orig,x_orig], [y_max,x_max], [256, 192], 20),
+        ])
+
+    if aug_type == "v0vl":
+        return transforms.Compose([
+            aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
+            aug.GridJitter(2, 0.1),
+            aug.JitterCoords(0.1),
+            RandomCentralRotation2D(90, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            RandomCentralShear2D(0.3, 0.3, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            RandomCentralStretch2D(0.2, 0.2, img_size=[y_orig, x_orig], center=[256,128], jitter=10, p=aug_prob),
+            aug.RandomScaleCharge(0.1, p=aug_prob),
+            aug.RandomJitterCharge(0.1, p=aug_prob),
+            LogAlphaCharge(5),
+            aug.BilinearSplatReduce(0.25, 0.4),
+            aug.RandomDropout(0.5, p=aug_prob),
+            RandomCenterCrop([y_orig,x_orig], [y_max,x_max], [256, 192], 20),
+        ])
+    
     if aug_type == "v0grid":
         return transforms.Compose([
             aug.RandomVerticalFlip(y_max=y_orig, p=0.5),
