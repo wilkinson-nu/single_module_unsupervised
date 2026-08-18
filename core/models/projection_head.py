e19d27edadfe8a2d3e3c6e3bc248ed481bad0edd
Callum Wilkinson <cwilkinson@lbl.gov>
2026-08-18 13:47:15 -0700

    Add more options to the projection head

--- core/models/projection_head.py
+++ core/models/projection_head.py
@@ -29,7 +29,9 @@ class ProjectionHeadTwoLayer(nn.Module):
                  nlatent: int,
                  nhidden: int = -1,
                  hidden_act_fn : object = nn.ReLU,
-                 apply_bn : bool = False):
+                 apply_bn : bool = False,
+                 init_bn : bool = False,
+                 final_bn : bool = True):
         super().__init__()
 
         ## Slightly dodgy to retain previous default behaviour
@@ -37,42 +39,61 @@ class ProjectionHeadTwoLayer(nn.Module):
         self.nlatent = nlatent
         self.nhidden = nhidden if nhidden != -1 else nchan//4
         self.apply_bn = apply_bn
+        self.init_bn = init_bn
+        self.final_bn = final_bn
         self.hidden_act_fn = hidden_act_fn
         
         self.proj = self.make_network()
         self.initialize_weights()
 
     def make_network(self):
+        ## BN: h -> BN(affine=True) -> Linear(bias=False) -> BN(affine=True)  -> ReLU -> Linear(bias=False) -> BN(affine=False) -> z
+        ## No BN: h -> BN(affine=True) -> Linear(bias=True) -> ReLU -> Linear(bias=False) -> z
         proj = OrderedDict()
-        proj['lin1'] = nn.Linear(self.nchan, self.nhidden, bias=True)
-        if self.apply_bn: proj['norm1'] = nn.BatchNorm1d(self.nhidden)
+        if self.init_bn:
+            proj['norm0'] = nn.BatchNorm1d(self.nchan, affine=True)
+
+        proj['lin1'] = nn.Linear(self.nchan, self.nhidden, bias=not self.apply_bn)
+        if self.apply_bn:
+            proj['norm1'] = nn.BatchNorm1d(self.nhidden, affine=True)
         proj['act1'] = self.hidden_act_fn()
+
         proj['lin2'] = nn.Linear(self.nhidden, self.nlatent, bias=False)
-        if self.apply_bn: proj['norm2'] = nn.BatchNorm1d(self.nlatent)
+        if self.apply_bn and self.final_bn:
+            proj['norm2'] = nn.BatchNorm1d(self.nlatent, affine=False)
+
         return nn.Sequential(proj)
-        
+
+    
     def initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity="linear")
-                if m.bias is not None: nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm1d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.BatchNorm1d, nn.SyncBatchNorm)):
+                if m.weight is not None:
+                    nn.init.ones_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
 
     def forward(self, x, return_hidden=False):
 
-        ## Separate execution so hidden layers can be returned
-        h1_logits = self.proj.lin1(x)
-        h1 = h1_logits
+        ## Separate execution so hidden layers can be returned                                                                                                                                                     
+        if self.init_bn:
+            x = self.proj.norm0(x)
 
+        h1 = self.proj.lin1(x)
         if self.apply_bn: h1 = self.proj.norm1(h1)
         h1 = self.proj.act1(h1)
+
         out = self.proj.lin2(h1)
-        if self.apply_bn: out = self.proj.norm2(out)
+        if self.apply_bn and self.final_bn:
+            out = self.proj.norm2(out)
 
         if return_hidden:
-            return {"proj_layer1": h1_logits,
+            return {"proj_input": x, "proj_layer1": h1,
                     "proj_final": out}
         return out
 
@@ -83,7 +104,9 @@ class ProjectionHeadThreeLayer(nn.Module):
                  nlatent: int,
                  nhidden: int = -1,
                  hidden_act_fn : object = nn.ReLU,
-                 apply_bn : bool = False):
+                 apply_bn : bool = False,
+                 init_bn : bool = False,
+                 final_bn : bool = True):
         super().__init__()
 
 
@@ -92,6 +115,8 @@ class ProjectionHeadThreeLayer(nn.Module):
         ## Slightly dodgy to retain previous default behaviour
         self.nhidden = nhidden if nhidden != -1 else nchan//4
         self.apply_bn = apply_bn
+        self.init_bn = init_bn
+        self.final_bn = final_bn
         self.hidden_act_fn = hidden_act_fn
 
         self.proj = self.make_network()
@@ -99,61 +124,74 @@ class ProjectionHeadThreeLayer(nn.Module):
 
     def make_network(self):
         proj = OrderedDict()
-        proj['lin1'] = nn.Linear(self.nchan, self.nhidden, bias=True)
-        if self.apply_bn: proj['norm1'] = nn.BatchNorm1d(self.nhidden)
+        if self.init_bn:
+            proj['norm0'] = nn.BatchNorm1d(self.nchan, affine=True)
+                
+        proj['lin1'] = nn.Linear(self.nchan, self.nhidden, bias=not self.apply_bn)
+        if self.apply_bn:
+            proj['norm1'] = nn.BatchNorm1d(self.nhidden, affine=True)
         proj['act1'] = self.hidden_act_fn()
-        proj['lin2'] = nn.Linear(self.nhidden, self.nhidden, bias=True)
-        if self.apply_bn: proj['norm2'] = nn.BatchNorm1d(self.nhidden)
+
+        proj['lin2'] = nn.Linear(self.nhidden, self.nhidden, bias=not self.apply_bn)
+        if self.apply_bn:
+            proj['norm2'] = nn.BatchNorm1d(self.nhidden, affine=True)
         proj['act2'] = self.hidden_act_fn()
+
         proj['lin3'] = nn.Linear(self.nhidden, self.nlatent, bias=False)
-        if self.apply_bn: proj['norm3'] = nn.BatchNorm1d(self.nlatent)
+        if self.apply_bn and self.final_bn:
+            proj['norm3'] = nn.BatchNorm1d(self.nlatent, affine=False)
+
         return nn.Sequential(proj)
+        
 
     def initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity="linear")
-                if m.bias is not None: nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm1d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.BatchNorm1d, nn.SyncBatchNorm)):
+                if m.weight is not None:
+                    nn.init.ones_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
 
 
     def forward(self, x, return_hidden=False):
 
         ## Separate execution so hidden layers can be returned
-        h1_logits = self.proj.lin1(x)
-        h1 = h1_logits
+        if self.init_bn:
+            x = self.proj.norm0(x)
 
+        h1 = self.proj.lin1(x)
         if self.apply_bn: h1 = self.proj.norm1(h1)
         h1 = self.proj.act1(h1)
-        h2_logits = self.proj.lin2(h1)
-        h2 = h2_logits
 
+        h2 = self.proj.lin2(h1)
         if self.apply_bn: h2 = self.proj.norm2(h2)
         h2 = self.proj.act2(h2)
-        out = self.proj.lin3(h2)
-        if self.apply_bn: out = self.proj.norm3(out)
-        
-        if return_hidden:
-            return {"proj_layer1": h1_logits,
-                    "proj_layer2": h2_logits,
-                    "proj_final": out}
-        return out
 
+        out = self.proj.lin3(h2)
+        if self.apply_bn and self.final_bn:
+            out = self.proj.norm3(out)
+
+        if return_hidden:
+            return {"proj_input": x, "proj_layer1": h1,
+                    "proj_layer2": h2, "proj_final": out}
+        return out
         
         
 def get_projhead(nchan, args):
     hidden_act_fn = get_act_from_string(args.enc_act)
     
     if args.proj_arch in ["logits", "two"]:
-        proj_head = ProjectionHeadTwoLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=False)
+        proj_head = ProjectionHeadTwoLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=False, init_bn=getattr(args, "proj_init_bn", False))
     elif args.proj_arch in ["logitsbn", "twobn"]:
-        proj_head = ProjectionHeadTwoLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=True)
+        proj_head = ProjectionHeadTwoLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=True, init_bn=getattr(args, "proj_init_bn", False))
     elif args.proj_arch == "three":
-        proj_head = ProjectionHeadThreeLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=False)
+        proj_head = ProjectionHeadThreeLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=False, init_bn=getattr(args, "proj_init_bn", False))
     elif args.proj_arch == "threebn":
-        proj_head = ProjectionHeadThreeLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=True)
+        proj_head = ProjectionHeadThreeLayer(nchan, args.latent, getattr(args, "nhidden", -1), hidden_act_fn, apply_bn=True, init_bn=getattr(args, "proj_init_bn", False))
     elif args.proj_arch == "one":
         proj_head = ProjectionHeadOneLayer(nchan, args.latent)
     return proj_head
