Fix world_size issue with gradient size

core/losses/gather.py
@@ -7,16 +7,12 @@ class GatherLayer(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, input):
-        ctx.save_for_backward(input)
         output = [torch.zeros_like(input) for _ in range(dist.get_world_size())]
         dist.all_gather(output, input)
         return tuple(output)
 
     @staticmethod
     def backward(ctx, *grads):
-        (input,) = ctx.saved_tensors
-        grad_out = torch.zeros_like(input)
-        ## A key gotcha here is that this only returns gradients for the current rank
-        ## So DDP deals with summing gradients correctly later
-        grad_out[:] = grads[dist.get_rank()]
-        return grad_out
+        all_grads = torch.stack(grads)
+        dist.all_reduce(all_grads)
+        return all_grads[dist.get_rank()]