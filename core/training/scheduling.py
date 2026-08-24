d390ba65dd8c1813fed12b71790b9ee90b138fe4
cwilkinson <cwilkinson@lbl.gov>
2026-08-24 16:19:18 -0700

    Separate out residual BN blocks

--- core/training/scheduling.py
+++ core/training/scheduling.py
@@ -3,6 +3,12 @@ import math
 from torch import optim
 from core.training.lars import LARS, LARS_LRScheduler
 
+def is_final_residual_gamma(name):
+    return (
+        name.endswith("norm3.bn.weight")  # v1 Bottleneck
+        or name.endswith("norm2.bn.weight")  # v1 BasicBlock
+    )
+
 def get_opt_and_sched(args, encoder, heads, total_steps, world_size, print_debug=False):
 
     lr_scheduler = None
@@ -69,16 +75,21 @@ def build_param_groups(encoder,
     enc_params  = []
     omit_params = []
     head_params = []
+    residual_gamma_params = []
 
     enc_names  = []
     omit_names = []
     head_names = []
+    residual_gamma_names = []
     
     for name, param in encoder.named_parameters():
         if not param.requires_grad:
             continue
-        
-        if param.ndim == 1 or name.endswith(".bias"):
+
+        if is_final_residual_gamma(name)
+            residual_gamma_params.append(param)
+            residual_gamma_names.append(name)
+        elif param.ndim == 1 or name.endswith(".bias"):
             omit_params.append(param)
             omit_names.append(name)
         else:
@@ -107,6 +118,10 @@ def build_param_groups(encoder,
         for name, param in zip(enc_names, enc_params):
             print(f"  LARS + WD:       {name:60s} {tuple(param.shape)}")
 
+        print(f"RES GAMMA ({len(residual_gamma_params)} parameters):")
+        for name, param in zip(residual_gamma_names, residual_gamma_params):
+            print(f"  LARS + WD:       {name:60s} {tuple(param.shape)}")        
+            
         print(f"OMITTED ({len(omit_params)} parameters):")
         for name, param in zip(omit_names, omit_params):
             print(f"  NO LARS / NO WD:  {name:60s} {tuple(param.shape)}")
@@ -121,6 +136,7 @@ def build_param_groups(encoder,
     
     return [
         {
+            "group_name": "enc",
             "params": enc_params,
             "names": enc_names,
             "weight_decay": weight_decay,
@@ -128,20 +144,31 @@ def build_param_groups(encoder,
             "lars_exclude": False
         },
         {
+            "group_name": "omit",
             "params": omit_params,
             "names": omit_names,
             "weight_decay": 0.0,
             "weight_sched": False,
             "lars_exclude": True,
-            "lr_scale": 1.0
+            "lr_scale": args.non_lars_lr_scale
         },
         {
+            "group_name": "head",
             "params": head_params,
             "names": head_names,
             "weight_decay": head_weight_decay,
             "weight_sched": weight_sched and weight_decay_head,
             "lars_exclude": False
         },
+        {
+            "group_name": "residual_gamma",
+            "params": residual_gamma_params,
+            "names": residual_gamma_names,
+            "weight_decay": 0.0,
+            "weight_sched": False,
+            "lars_exclude": True,
+            "lr_scale": args.non_lars_lr_scale,
+        }
     ]
 
 ## This is essentially redundant code used by the DINO implementation, remove at some point to use a consistent scheduler
