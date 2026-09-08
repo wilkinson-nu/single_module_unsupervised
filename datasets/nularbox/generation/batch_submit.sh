f7638257dda227b530faee9ed47e189926b83c18
Callum Wilkinson <cwilkinson@lbl.gov>
2026-09-08 13:48:29 -0700

    Update generation script

--- datasets/nularbox/generation/batch_submit.sh
+++ datasets/nularbox/generation/batch_submit.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
 ## Control the number of jobs to spawn
-FIRST_JOB=0
-LAST_JOB=9
+FIRST_JOB=400
+LAST_JOB=999
 
-## Control the jobs
-OUTDIR_ROOT="/global/cfs/cdirs/dune/users/cwilk/nularbox_simulation"
+## DUNE FLUX
+#EXPT_NAME="DUNEND"
+#FLUX_FILE="DUNE_OptimizedEngineeredNov2017_REGULAR.root"
+#FLUX_HIST="numu_NDFHC_flux"
 
+## NuMI ME flux
+EXPT_NAME="NuMIME"
 FLUX_FILE="MINERvA_flux_ME1F.root"
 FLUX_HIST="flux_E_cvweighted_CV_WithStatErr"
+
 TARG="1000180400[1.00]"
 NU_PDG=14
 E_MIN=0.1
 E_MAX=50.0
-GEOM=argon_box_2m.gdml
-NEVENTS=5000
+GEOM=argon_box_3m.gdml
+NEVENTS=25000
 EDEP_MAC=edep.mac
 
-FLUX_NAME=NuMIME
-GEN_NAME=GENIE10a
-TUNE=G18_10a_00_000
+GEN_NAME=GENIE10c
+TUNE=G18_10c_00_000
 TEMPLATE=batch_GENIEv3_${TUNE}_EDEPSIM_2D_TEMPLATE.sh
 
 ## image making
 IMAGE_SIZE=512
-MIN_HITS=1
-THRESHOLD=0.2
+EXIT_DOWNSTREAM=1
+MIN_HITS=10
+THRESHOLD=0
+
+## Directory to save to
+OUTDIR_ROOT="/global/cfs/cdirs/dune/users/cwilk/nularbox_simulation/GENIEv3_${TUNE}_${EXPT_NAME}"
 
 ## Loop over jobs
 for N in $(seq ${FIRST_JOB} ${LAST_JOB})
 do
     printf -v PADJOB "%03d" ${N}
 
-    OUTFILE_ROOT=nularbox_${FLUX_NAME}_${NU_PDG}_${GEN_NAME}_${PADJOB}
+    OUTFILE_ROOT=nularbox_${EXPT_NAME}_${NU_PDG}_${GEN_NAME}_${PADJOB}
 
     echo "Processing ${OUTFILE_ROOT}"
 
@@ -54,6 +62,7 @@ do
     sed -i "s/__NEVENTS__/${NEVENTS}/g" ${THIS_TEMP}
     sed -i "s/__EDEP_MAC__/${EDEP_MAC}/g" ${THIS_TEMP}
     sed -i "s/__IMAGE_SIZE__/${IMAGE_SIZE}/g" ${THIS_TEMP}
+    sed -i "s/__EXIT_DOWNSTREAM__/${EXIT_DOWNSTREAM}/g" ${THIS_TEMP}
     sed -i "s/__MIN_HITS__/${MIN_HITS}/g" ${THIS_TEMP}
     sed -i "s/__THRESHOLD__/${THRESHOLD}/g" ${THIS_TEMP}
     
