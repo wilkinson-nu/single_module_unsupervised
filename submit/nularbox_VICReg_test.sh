b41aa447f554276e463740e422b8baa1624acc76
Callum Wilkinson <cwilkinson@lbl.gov>
2026-09-18 16:39:07 -0700

    Add a simple test training script

---
+++ submit/nularbox_VICReg_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+## Get an interactive job to run this with:
+# salloc --nodes 1 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=32 --qos interactive --time 04:00:00 --constraint gpu --account dune
+IMAGE=docker:wilkinsonnu/ml_tools:ME
+REPO=$HOME/single_module_unsupervised
+source "$REPO/submit/common.sh"
+
+## Setup the run directory
+JOBNAME=contrastive_testing_vicreg
+RUN_DIR=$PSCRATCH/larch_test_runs/${SLURM_JOB_ID:-$(date +%Y%m%d_%H%M%S)}_${JOBNAME}
+snapshot_repo "$RUN_DIR" "$REPO"
+
+## Basic inputs and outputs
+DATA_DIR=$PSCRATCH/NULARBOX/GENIE10c_DUNEND_CCCONT256
+LOGFILE=log_${JOBNAME}
+STATE_FILE=state_${JOBNAME}.pth
+
+## Get the basic config here:
+CONFIG=$RUN_DIR/configs/default_nularbox_vicreg.yaml
+
+## Run specific arguments
+NEVENTS=200000
+NEPOCH=50
+
+## Do the business
+srun --cpu-bind=cores shifter --image=${IMAGE} \
+     python3 -m larch.experiments.nularbox_contrastive \
+     --config=${CONFIG} \
+     --run_dir=${RUN_DIR} \
+     --data_dir=${DATA_DIR} \
+     --log=${LOGFILE} \
+     --state_file=${STATE_FILE} \
+     --nevents=${NEVENTS} \
+     --nepoch=${NEPOCH}
+
