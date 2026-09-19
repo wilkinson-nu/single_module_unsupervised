ef375d3b0edf1dba24be48f0bd52f680da1cfb00
cwilkinson <cwilkinson@lbl.gov>
2026-09-19 09:56:50 -0700

    Catch missed import

--- src/larch/optim/lars.py
+++ src/larch/optim/lars.py
@@ -3,7 +3,7 @@ from math import cos, pi
 
 import math
 import torch
-from larch.core.training.logging import log_scalar
+from larch.training.logging import log_scalar
 
 class LARS(torch.optim.Optimizer):
     def __init__(
